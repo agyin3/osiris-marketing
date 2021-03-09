d34e69373ae57ac539591545f1301fc6f40ca185
Buddy Agyin <agyin3@gmail.com>
2021-03-09 04:58:59 -0700

    update link import and title meta tag

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
   return (
     <div className="bg-image bg-homepage-hero text-white text-roboto">
       <Head>
-        <title>Create Next App</title>
+        <title>Osiris Marketing Group</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
